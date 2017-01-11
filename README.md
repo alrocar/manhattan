3e6ccd235d12a8f7a15d9aec66a237855703efb8
Alberto Romeu <alberto.romeu.carrasco@gmail.com>
2017-01-11 21:34:55 +0100

    simple range thematic support

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 TODO
 
+- Make it work
 - Use WebWorkers to load data in the background
-- Include in the SQL the_geom and the field necessary for a thematic
-- Add thematic support in the drawing
-- Add an input for the SQL and a button to request data
-- Add progress bar or icon when loading the dataset
 - Implement panning
 - Add zoom buttons
-- Add mousewheel support for zooming
-- Add continuous zoom support
 - Generate vector tiles and index to improve drawing speed
 - Add support for geometry simplification to improve drawing speed
 - Measure and compare rendering times
 - Add double buffer in the canvas
 - Add base layer (tiles) support
+- Add mousewheel support for zooming
+- Add continuous zoom support
+- Include in the SQL the_geom and the field necessary for a thematic
+- Add thematic support in the drawing
+- Add an input for the SQL and a button to request data
+- Add progress bar or icon when loading the dataset
+- Make it nice
--- js/main.js
+++ js/main.js
@@ -10,7 +10,11 @@ var mapCenter = {
     y: 40.781841
 };
 
-var zoomLevel = 14;
+var zoomLevel = 12;
+var thematicAttribute = 'lot';
+var thematicLevels = 7;
+var thematicOffset;
+var colorRamp = ['#FFFFCC', '#C7E9B4', '#7FCDBB', '#41B6C4', '#1D91C0', '#225EA8', '#0C2C84'];
 
 function calculateResolutions(zoomLevels) {
     // var maxResolution = 20037508 * 2 / 256,
@@ -60,14 +64,14 @@ function degrees2meters(lon, lat) {
     };
 };
 
-var dataset, mapWidth, mapHeight;
+var /*dataset,*/ mapWidth, mapHeight;
 
 $(document).ready(function() {
     calculateResolutions(MAX_ZOOM_LEVEL);
 
 
-    $.getJSON(dataUrl, function(data) {
-        dataset = data;
+    // $.getJSON(dataUrl, function(data) {
+        // dataset = data;
         var canvas = $('#map');
         canvas.attr({ width: canvas.width(), height: canvas.height() })
 
@@ -91,7 +95,7 @@ $(document).ready(function() {
 
         canvas.on("mousedown", function(e) {
             canvas.on("mousemove", function(e) {
-                console.log(e.pageY + ',' + e.pageX);
+                // console.log(e.pageY + ',' + e.pageX);
                 // var newMapCenter = toCoordinate({x: e.pageX, y: e.pageY});
                 // draw(ctx, newMapCenter);
             });
@@ -100,9 +104,31 @@ $(document).ready(function() {
             // draw(ctx, mapCenter);
         });
 
+        prepare(dataset);
+
         draw(ctx, mapCenter);
     });
-});
+// });
+
+function prepare(dataset) {
+	var feature;
+	var minRange = Number.MAX_VALUE, maxRange = Number.MIN_VALUE;
+	for (var i = 0, l = dataset.features.length; i < l; i++) {
+        feature = dataset.features[i];
+
+        if (feature.properties[thematicAttribute]) {
+        	if (feature.properties[thematicAttribute] < minRange) {
+        		minRange = feature.properties[thematicAttribute];
+        	}
+
+        	if (feature.properties[thematicAttribute] > minRange) {
+        		maxRange = feature.properties[thematicAttribute];
+        	}
+        }
+    }
+
+    thematicOffset = (maxRange - minRange) / thematicLevels;
+};
 
 function draw(ctx, mapCenter) {
     empty(ctx);
@@ -114,16 +140,15 @@ function draw(ctx, mapCenter) {
     var minY = (mapCenter.y - (mapHeight / 2 * currentResolution));
     var maxY = (mapCenter.y + (mapHeight / 2 * currentResolution));
 
-    console.log(minX);
-    console.log(maxX);
-    console.log(minY);
-    console.log(maxY);
+    // console.log(minX);
+    // console.log(maxX);
+    // console.log(minY);
+    // console.log(maxY);
 
     // ctx.fillStyle = "rgb(200,0,0)";
     for (var i = 0, l = dataset.features.length; i < l; i++) {
+    	console.log(i);
         feature = dataset.features[i];
-
-        console.log(feature.geometry.type);
 
         //       ctx.beginPath();
         // ctx.moveTo(10,50);
@@ -131,21 +156,24 @@ function draw(ctx, mapCenter) {
         // ctx.lineTo(100,25);
         // ctx.fill();
 
+        var colorIndex = feature.properties[thematicAttribute] / thematicOffset + 1;
+        var color = colorRamp[Math.floor(colorIndex)];
         var polygon, coord, pixel;
         for (var j = 0, ll = feature.geometry.coordinates[0].length; j < ll; j++) {
             polygon = feature.geometry.coordinates[0][j];
+            ctx.fillStyle = color;
             ctx.beginPath();
             for (var k = 0, lll = polygon.length; k < lll; k++) {
                 coord = polygon[k];
 
-                console.log('map: ' + coord[0] + ',' + coord[1]);
+                // console.log('map: ' + coord[0] + ',' + coord[1]);
                 coord = degrees2meters(coord[0], coord[1]);
                 pixel = toPixel({
                     x: coord.x,
                     y: coord.y
                 }, minX, maxY);
 
-                console.log(pixel.x + ',' + pixel.y);
+                // console.log(pixel.x + ',' + pixel.y);
 
                 if (k == 0) {
                     ctx.moveTo(pixel.x, pixel.y);
